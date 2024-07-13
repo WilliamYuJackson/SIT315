358d2a79bb1105c4c5bf0a6185938597254a4ace
WilliamYuJackson <165125587+WilliamYuJackson@users.noreply.github.com>
2024-07-13 11:50:12 +1000

    Update m1_t1p1.ino

--- m1_t1p1.ino
+++ m1_t1p1.ino
@@ -2,26 +2,30 @@ const int motionSensorPin = 4; // Connects to the Pir Sensor Output pin
 const int redLedMotionPin = 13; // Red LED Connects to the 13 (indication motion)
 const int greenLedNoMotionPin = 12; // Green LED Connects to the 13 (indication No motion)
 
-void setup() {
+void setup() 
+{
   pinMode(motionSensorPin, INPUT); // Sets Motion Sensor as Input
   pinMode(redLedMotionPin, OUTPUT); // Sets Red LED pin as output
   pinMode(greenLedNoMotionPin, OUTPUT); // Sets Green LED pin as output
   Serial.begin(9600); // Initialize serial communication
 }
 
-void loop() {
+void loop() 
+{
   int motionState = digitalRead(motionSensorPin); // Read the state of the motion sensor
   Serial.print("Motion sensor state: ");
   Serial.println(motionState); // Print the raw sensor state
 
-  if (motionState == HIGH) {
+  if (motionState == HIGH) 
+  {
     digitalWrite(redLedMotionPin, HIGH); // Turn on the Red LED
     digitalWrite(greenLedNoMotionPin, LOW); // Turn off the Green LED
     Serial.println("Motion detected! Red LED is ON.");
-  } else {
+  } 
+  else 
+  {
     digitalWrite(redLedMotionPin, LOW); // Turn off the Red LED
     digitalWrite(greenLedNoMotionPin, HIGH); // Turn on the Green LED
     Serial.println("No motion. Green LED is ON.");
   }
-  delay(100); // Wait for a 1/10 of second
-}
+  delay(500); // Wait for a 500ms
